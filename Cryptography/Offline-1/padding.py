9c19ed5d7c87ae14630f19ec156b69192d9ee1f7
amimulamim <amimul.ehsan2001@gmail.com>
2025-04-25 11:33:21 +0600

    padding added

---
+++ Cryptography/Offline-1/padding.py
@@ -0,0 +1,17 @@
+
+#using CBC with PKCS7 padding
+
+def pad_pkcs7(data: bytes, block_size: int = 16) -> bytes:
+	pad_length = block_size - (len(data) % block_size)
+	padding = bytes([pad_length] * pad_length)
+	return data + padding
+
+def unpad_pkcs7(data: bytes) -> bytes:
+	if not data:
+		return data
+	pad_length = data[-1]
+	if pad_length < 1 or pad_length > 16:
+		raise ValueError("Invalid padding length")
+	if data[-pad_length:] != bytes([pad_length] * pad_length):
+		raise ValueError("Invalid padding")
+	return data[:-pad_length]
