897bd1c0b159a73b03a864cf6c6b9b55a08610dc
amimulamim <amimul.ehsan2001@gmail.com>
2025-06-20 10:44:53 +0600

    deploy advanced

--- Side Channel Attack/starter_code/template/deploy.sh
+++ Side Channel Attack/starter_code/template/deploy.sh
@@ -1,4 +1,6 @@
 scp -r -i ../../../SEED_key.pem saved_models seed@20.40.60.232:/home/seed/src-cloud/Side\ Channel\ Attack/starter_code/template/
+scp -r -i ../../../SEED_key.pem saved_models_advanced seed@20.40.60.232:/home/seed/src-cloud/Side\ Channel\ Attack/starter_code/template/
+
 
 ssh -i ../../../SEED_key.pem seed@20.40.60.232
 
